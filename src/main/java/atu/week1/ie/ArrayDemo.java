c71f23edeeb7a0c3d6ee1fa3ea5bb5eeac8e92db
G00405014 <G00405014@atu.ie>
2023-01-30 11:56:01 +0000

    Printing Arrays

--- src/main/java/atu/week1/ie/ArrayDemo.java
+++ src/main/java/atu/week1/ie/ArrayDemo.java
@@ -3,23 +3,31 @@ package atu.week1.ie;
 public class ArrayDemo {
     public static void main(String[] args) {
 
-        int[] anArray;
+        int[] Array;
 
-        anArray =  new int[10];
+        Array =  new int[10];
 
-        anArray[0] = 100;
-        anArray[1] = 200;
-        anArray[2] = 300;
-        anArray[3] = 400;
-        anArray[4] = 500;
-        anArray[5] = 600;
-        anArray[6] = 700;
-        anArray[7] = 800;
-        anArray[8] = 900;
-        anArray[9] = 1000;
+        Array[0] = 100;
+        Array[1] = 200;
+        Array[2] = 300;
+        Array[3] = 400;
+        Array[4] = 500;
+        Array[5] = 600;
+        Array[6] = 700;
+        Array[7] = 800;
+        Array[8] = 900;
+        Array[9] = 1000;
 
-
-
+        System.out.println("Print Element at index 0 " + Array[0]);
+        System.out.println("Print Element at index 1 " + Array[1]);
+        System.out.println("Print Element at index 2 " + Array[2]);
+        System.out.println("Print Element at index 3 " + Array[3]);
+        System.out.println("Print Element at index 4 " + Array[4]);
+        System.out.println("Print Element at index 5 " + Array[5]);
+        System.out.println("Print Element at index 6 " + Array[6]);
+        System.out.println("Print Element at index 7 " + Array[7]);
+        System.out.println("Print Element at index 8" + Array[8]);
+        System.out.println("Print Element at index 9 " + Array[9]);
 
 
     }
