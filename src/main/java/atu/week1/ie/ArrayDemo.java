6da757b836f72526ab7bfce420773ac2fe941d6f
G00405014 <G00405014@atu.ie>
2023-01-30 12:43:11 +0000

    try catch

--- src/main/java/atu/week1/ie/ArrayDemo.java
+++ src/main/java/atu/week1/ie/ArrayDemo.java
@@ -1,26 +1,49 @@
 package atu.week1.ie;
 
+import java.util.Scanner;
+
 public class ArrayDemo {
     public static void main(String[] args) {
 
-        int[] anArray;
+        int[] Array;
 
-        anArray =  new int[10];
-
-        anArray[0] = 100;
-        anArray[1] = 200;
-        anArray[2] = 300;
-        anArray[3] = 400;
-        anArray[4] = 500;
-        anArray[5] = 600;
-        anArray[6] = 700;
-        anArray[7] = 800;
-        anArray[8] = 900;
-        anArray[9] = 1000;
+        Array = new int[10];
 
 
+        Array[0] = 100;
+        Array[1] = 200;
+        Array[2] = 300;
+        Array[3] = 400;
+        Array[4] = 500;
+        Array[5] = 600;
+        Array[6] = 700;
+        Array[7] = 800;
+        Array[8] = 900;
+        Array[9] = 1000;
 
+        System.out.println("Print Element at index 0 " + Array[0]);
+        System.out.println("Print Element at index 1 " + Array[1]);
+        System.out.println("Print Element at index 2 " + Array[2]);
+        System.out.println("Print Element at index 3 " + Array[3]);
+        System.out.println("Print Element at index 4 " + Array[4]);
+        System.out.println("Print Element at index 5 " + Array[5]);
+        System.out.println("Print Element at index 6 " + Array[6]);
+        System.out.println("Print Element at index 7 " + Array[7]);
+        System.out.println("Print Element at index 8" + Array[8]);
+        System.out.println("Print Element at index 9 " + Array[9]);
 
+        int indexValue = 0;
+
+        for (int value; Array; ) {
+            System.out.println("Element at index" + index + ":" + value);
+            indexValue++;
+        }
+        Scanner myScan = new Scanner();
+        try {
+            System.out.println("Please enter a value ");
+            Array[10] = myScan.nextInt();
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("inccorect array value");
+        }
     }
-
 }
---
+++ src/main/java/atu/week1/ie/ArrayListDemo.java
@@ -0,0 +1,7 @@
+package atu.week1.ie;
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+
+    }
+}
